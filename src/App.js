a4d75fe619c97e251eb09a09e3dd90e5080f4f71
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-03-15 11:31:23 -0300

    GR-test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
-import styled from 'styled-components';
+import "./App.css";
+import styled from "styled-components";
 
 const Button = styled.button`
   padding: 0.3em 1em;
@@ -7,14 +7,16 @@ const Button = styled.button`
   background-color: slateblue;
   color: white;
   border-radius: 5px;
-`
+`;
 
 function App() {
   return (
     <div className="App">
       <h1>GBM Ingeniería y Ambiente</h1>
       <Button>Botón con styled components</Button>
-      <button type='button' className='btn btn-success'>Botón con Bootstrap</button>
+      <button type="button" className="btn btn-success">
+        Botón con Bootstrap 5
+      </button>
     </div>
   );
 }
