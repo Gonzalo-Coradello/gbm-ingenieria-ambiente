5c530c41b06df384e05469126e3554bddcfd3de9
Gonzalo Ramos <gonzaloalexramos@gmail.com>
2023-03-15 20:39:55 -0300

    font family added

--- src/App.js
+++ src/App.js
@@ -10,13 +10,28 @@ const Button = styled.button`
 `;
 
 function App() {
+  const navbarLinks = ["Quienes somos", "Servicios", "Contacto"];
   return (
     <div className="App">
-      <h1>GBM Ingeniería y Ambiente</h1>
+      <header className="d-flex align-items-center bor-2r justify-content-between px-5 bg-primary py-2">
+        <h1 className="bor-1r m-0 p-0">GBM Ingeniería y Ambiente</h1>
+        <nav className="bor-2b">
+          <ul className="d-flex gap-flex-5 m-0">
+            {navbarLinks.map((link) => {
+              return (
+                <li className="bor-1r ml-2" key={link}>
+                  {link}
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
       <Button>Botón con styled components</Button>
       <button type="button" className="btn btn-success">
-        Botón con Bootstrap V
+        Botón con Bootstrap 4
       </button>
+     
     </div>
   );
 }
