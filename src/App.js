37a2e10f22c9bf1a326f0f5cf838bd67811bbbb8
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-03-16 10:37:20 -0300

    add projects section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Projects />
     </div>
   );
 }
