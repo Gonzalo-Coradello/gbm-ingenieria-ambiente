20540db35dea9bc2489db6c077f743ff8a516f25
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-01 11:36:21 -0300

    create animated cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,80 @@
-const Card = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import styled from "styled-components";
 
-export default Card
+const Card = ({ service: { title, body1, body2, image } }) => {
+  return (
+    <StyledCard>
+      <StyledFront>
+        <h3>{title}</h3>
+        <img src={image} alt={title} />
+      </StyledFront>
+      <StyledBack>
+        <h3>{title}</h3>
+        <p>{body1}</p>
+        <p>{body2}</p>
+        <img src={image} alt={title} />
+      </StyledBack>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled.div`
+  position: relative;
+  width: 348px;
+  height: 378px;
+  border-radius: 2.5rem;
+  color: #fff;
+
+  & div {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    padding: 4rem 4rem;
+    backface-visibility: hidden;
+    overflow: hidden;
+    transition: 500ms ease-in-out;
+  }
+
+  & h3 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
+  & p {
+    line-height: 1.15;
+  }
+
+  & img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 2.5rem;
+    z-index: -10;
+  }
+
+  &:hover div:nth-child(1) {
+    transform: perspective(1000px) rotateY(180deg);
+  }
+
+  &:hover div:nth-child(2) {
+    transform: perspective(1000px) rotateY(360deg);
+  }
+`;
+
+const StyledFront = styled.div`
+  transform: perspective(1000px) rotateY(0deg);
+`;
+
+const StyledBack = styled.div`
+  transform: perspective(1000px) rotateY(180deg);
+
+  & img {
+    transform: rotateY(180deg);
+    filter: brightness(40%);
+  }
+`;
+
+export default Card;
--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,12 +1,42 @@
-import Card from "../Card/Card"
+import styled from "styled-components";
+import Card from "../Card/Card";
 
+const services = [
+  {
+    title: "Asesoría",
+    body1: "Asesorar a emprendedores y desarrolladores inmobiliarios sobre factibilidades y viabilidad del proyecto.",
+    body2: "Acompañamiento a clientes en la dirección y ejecución de las obras de urbanización.",
+    image: "/images/services/service.png"
+  },
+  {
+    title: "Desarrollo",
+    body1: "Desarrollo de proyecto de loteo y urbanización.",
+    body2: "Desarrollo de proyectos de redes de servicios, como redes de agua potable y cloaca, de electricidad y de gas.",
+    image: "/images/services/service.png"
+  },
+  {
+    title: "Estudios",
+    body1: "Ejecución de estudios ambientales, estudios hídricos e hidráulicos",
+    body2: "",
+    image: "/images/services/service.png"
+  },
+];
 
 const Services = () => {
   return (
-    <div>
-      <Card />
-    </div>
-  )
-}
+    <StyledDiv>
+      {services.map((service) => (
+        <Card key={service.title} service={service} />
+      ))}
+    </StyledDiv>
+  );
+};
 
-export default Services
+const StyledDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+export default Services;
