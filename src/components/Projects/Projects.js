1e10d5aca0a21222c5a42432a38c54e9aa93e193
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-05 11:09:30 -0300

    change images

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,18 +5,17 @@ import "swiper/css/autoplay";
 import styled from "styled-components";
 
 const images = [
-  { src: "http://placekitten.com/300/300", alt: "img1" },
-  { src: "http://placekitten.com/300/300", alt: "img2" },
-  { src: "http://placekitten.com/300/300", alt: "img3" },
-  { src: "http://placekitten.com/300/300", alt: "img4" },
-  { src: "http://placekitten.com/300/300", alt: "img5" },
-  { src: "http://placekitten.com/300/300", alt: "img6" },
-  { src: "http://placekitten.com/300/300", alt: "img7" },
-  { src: "http://placekitten.com/300/300", alt: "img8" },
-  { src: "http://placekitten.com/300/300", alt: "img9" },
-  { src: "http://placekitten.com/300/300", alt: "img10" },
-  { src: "http://placekitten.com/300/300", alt: "img11" },
-  { src: "http://placekitten.com/300/300", alt: "img12" }
+  { src: "/images/projects/project1.png", alt: "Proyecto 1" },
+  { src: "/images/projects/project2.png", alt: "Proyecto 2" },
+  { src: "/images/projects/project3.png", alt: "Proyecto 3" },
+  { src: "/images/projects/project4.png", alt: "Proyecto 4" },
+  { src: "/images/projects/project5.png", alt: "Proyecto 5" },
+  { src: "/images/projects/project6.png", alt: "Proyecto 6" },
+  { src: "/images/projects/project7.png", alt: "Proyecto 7" },
+  { src: "/images/projects/project8.png", alt: "Proyecto 8" },
+  { src: "/images/projects/project9.png", alt: "Proyecto 9" },
+  { src: "/images/projects/project3.png", alt: "Proyecto 10" },
+  { src: "/images/projects/project5.png", alt: "Proyecto 11" }
 ];
 
 const Projects = () => {
@@ -85,6 +84,19 @@ const RoundImg = styled.div`
 const Container = styled.div`
   max-width: 1200px;
   margin-inline: auto;
+  padding-inline: 1rem;
+
+  & img {
+    margin-left: 1.3rem;
+
+    @media (min-width: 500px) {
+      margin-left: 2rem;
+    }
+
+    @media (min-width: 1200px) {
+      margin-left: 0;
+    }
+  }
 `;
 
 export default Projects;
