a5c112f03042c510a9bf4700d834deedff99a9e3
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-23 12:44:54 -0300

    modify about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,26 @@
 import styled from "styled-components";
+import MainButton from "../Buttons/MainButton";
 
 const About = () => {
   return (
     <div id="about">
       <Heading>Quiénes somos</Heading>
       <StyledDiv>
-        <Card>
-            <p>Somos una consultora de jóvenes profesionales gestada en el año 2020, encabezada por un Ingeniero Civil y un Licenciado en Gestión Ambiental, en colaboración con una red de profesionales satélites, entre ellos: arquitectos, geógrafos y agrimensores. Nos dedicamos a la asesoría, gestión y diseño de proyectos inmobiliarios para grupos desarrolladores y emprendedores privados.</p>
-            <p>Nuestra labor surgió al identificar la problemática de la trazabilidad de un proyecto de loteo y urbanización. Se observó cierta desvinculación en una serie de entidades y procesos administrativos y profesionales, por los que el proyecto debe pasar para su puesta en marcha y su final desarrollo, generando como consecuencia que el proceso de desplegar un proyecto de loteo y fraccionamiento sea tedioso y extenso en tiempo.</p>
-            <p>Nuestra misión es acompañar al cliente en el proceso, nucleando todos los procesos técnicos y administrativos en un solo actor, y de esta forma, generar que los desarrollos inmobiliarios sean efectivos en su materialización y agiles en el tiempo.</p>
-        </Card>
-        <img src="/images/about-rounded.png" alt="Ingeniería y ambiente" />
+        <p className="font-italic">
+          Grupo GBM, es una consultora, constructora y desarrolladora de
+          proyectos inmobiliarios, de comercio y sector industrial tanto
+          públicos como privados.
+        </p>
+        <p className="font-italic">
+          Nacida en el año 2020, surgiendo de la necesidad de los posibles
+          clientes para el acompañamiento de sus proyectos, generando confianza
+          para llevar procesos administrativos y profesionales, con mayor
+          eficacia, resoluciones a tiempo y calidad eficiente.{" "}
+        </p>
       </StyledDiv>
+      <ButtonContainer>
+        <MainButton size="large">Saber más</MainButton>
+      </ButtonContainer>
     </div>
   );
 };
@@ -40,38 +49,21 @@ const Heading = styled.h2`
       }
 `;
 
-const Card = styled.div`
-    background-color: var(--green);
-    color: #FFF;
-    max-width: 568px;
-    display: grid;
-    justify-content: center;
-    margin-inline: auto;
-    padding: 2rem;
-    padding-bottom: .5rem;
-    z-index: 10;
-    font-size: 15px;
-    line-height: 15px;
-`
-
 const StyledDiv = styled.div`
-    display: flex;
-    flex-direction: column-reverse;
-    justify-content: center;
-    
-    @media (min-width: 980px) {
-      flex-direction: row;
-      gap: 2rem;
-      align-items: center;
-      justify-content: stretch;
-      margin-top: 4rem;
+  max-width: 70ch;
+  margin-inline: auto;
+  padding-inline: 2rem;
+  font-size: 20px;
+`;
 
-      & > img {
-        margin-left: -4rem;
-        border-radius: 2.5rem;
-      }
-    }
-`
+const ButtonContainer = styled.div`
+  display: grid;
+  justify-content: center;
+  margin-right: 2rem;
+  font-size: 16px;
 
+  @media (min-width: 650px) {
+    justify-content: end;
+`;
 
 export default About;
