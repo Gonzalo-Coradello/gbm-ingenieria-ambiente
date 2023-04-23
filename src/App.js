828a65eeb6cb35dd2d64637825247ee70d35303c
Gonzalo Coradello <gonzalocoradello@gmail.com>
2023-04-23 12:43:16 -0300

    add react router dom

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import "./App.css";
 import Footer from "./layouts/Footer/Footer";
 import Navbar from "./layouts/Navbar/Navbar"
 import Main from "./layouts/Main/Main";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Main />
-      <Footer />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          {/* <Route path="/servicios" element={<Services />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
